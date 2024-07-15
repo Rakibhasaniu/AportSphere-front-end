8f34d34e260f1200d93be432683d2f95ab561d8e
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-07-15 23:44:56 +0600

    login form created

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+    const {register, handleSubmit} = useForm();
+    const onSubmit = () => {
+        console.log('Form submitted');
+    }
     return (
-        <div>
-            <h1>This is login page</h1>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label htmlFor="id">ID:</label>
+                <input type="text" id="id" {...register('id')} />
+            </div>
+            <div>
+                <label htmlFor="password">PASSWORD:</label>
+                <input type="text"  id="password" {...register('password')} />
+            </div>
+            <Button onSubmit={sub} >submit</Button>
+        </form>
     );
 };
 
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,19 +2,23 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 
 
-const baseApi = createApi({
+export const baseApi = createApi({
     reducerPath:'baseApi',
     baseQuery: fetchBaseQuery({
         baseUrl:'http://localhost:5000/api/v1'
     }),
-    endpoints:(builder) => ({
-        login:builder.mutation({
-            query:(userInfo)=> ({
-                url:'/user/login',
-                method:'POST',
-                body:userInfo
+    endpoints:() => ({
+        // loginUser:builder.mutation({
+        //     query:(userInfo)=> ({
+        //         url:'/user/login',
+        //         method:'POST',
+        //         body:userInfo
 
-            })
-        })
+        //     })
+        // })
     })
 })
+
+// export const { useLoginMutation } = baseApi as {
+//     useLoginMutation: () => [any, { error: FetchBaseQueryError, isLoading: boolean }];
+// };
