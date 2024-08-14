7acb8f3f42a3be354caea64fc3ef929c09641129
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-08-14 14:32:24 +0600

    header added by resux

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,13 +1,26 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
 
 const baseQuery =  fetchBaseQuery({
     baseUrl:'http://localhost:5000/api/v1',
-    credentials:'include'
+    credentials:'include',
+    prepareHeaders:(headers,{getState})=> {
+        const token = (getState() as RootState).auth.token;
+        if(token){
+            headers.set('Authorization',`${token}`)
+        }
+        return headers;
+    }
 })
+
+
+const baseQueryWithRefreshToken = async(args, api, extraOptions) => {
+    const result = await baseQuery(args,api,extraOptions)
+}
 
 export const baseApi = createApi({
     reducerPath:'baseApi',
-    baseQuery:baseQuery,
+    baseQuery:baseQueryWithRefreshToken,
     endpoints:() => ({
         // loginUser:builder.mutation({
         //     query:(userInfo)=> ({
